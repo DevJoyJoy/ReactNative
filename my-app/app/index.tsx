8358eff690d781ca1584343c80b3be2dcf4af5eb
DevJoyJoy <joycenascimento2007@gmail.com>
2025-10-30 11:40:04 -0300

    login

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,12 +1,18 @@
-import { StyleSheet, View, Text } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View, Text, TextInput,TouchableOpacity, Button, Image } from "react-native";
 
 
 
-export default function HomeScreen() {
+export default function App() {
+  const [text, setText] = useState()
+
   return (
-    <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-      <View style={styles.square}></View>
-      <Text style={styles.colorBlue}>Hello World!!</Text>
+    <View style={{flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'thistle'}}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput placeholder="E-mail" onChangeText={(e) => setText(text)} style={styles.box}/>
+      <TextInput placeholder="Senha" onChangeText={(e) => setText(text)} style={styles.box}/>
+      <Button title="Entrar" onPress={() => console.log("ola")} style={styles.button}></Button>
+      {/* <Image style={styles.image} source={require('../assets/images/tokomaru.png')}></Image> */}
     </View>
   );
 }
@@ -20,5 +26,24 @@ const styles = StyleSheet.create({
     backgroundColor: "rebeccapurple",
     width: 300,
     height: 300
+  },
+  image: {
+    height: 300,
+    width: 300,
+  },
+  title: {
+    fontSize: 40,
+    color: 'rebeccapurple'
+  },
+  box: {
+    backgroundColor: "silver",
+    width: 270,
+    height: 35,
+    margin: 10,
+    borderRadius: 10,
+    padding: 8
+  },
+  button: {
+    backgroundColor: 
   }
 })
